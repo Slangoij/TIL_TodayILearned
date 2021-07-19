da7c74dbeed4f411b0ab0553ad152d06edf91bd3
slangoij <71580318+Slangoij@users.noreply.github.com>
2021-07-19 21:40:43 +0900

    210719-Algo_Study_with_yws,kpr start!

---
+++ 00.Algorithm/Backjoon/9024.두수의합.py
@@ -0,0 +1,9 @@
+t = int(input())
+for _ in range(t):
+    n, k = map(int, input().split())
+    soo_list = list(map(int, input().split()))
+
+    lft, rgt = 0, n-1
+
+    while lft < rgt:
+        pass
