ecc3b9cddc255ddc70085ef02b263cae68a62436
ij_slango <junginje@gmail.com>
2021-08-17 22:33:48 +0900

    210817-알고-Gen

--- 00.Algorithm/Backjoon/String/1464.뒤집기3.py
+++ 00.Algorithm/Backjoon/String/1464.뒤집기3.py
@@ -19,3 +19,20 @@ BCDAF
 
 
 """
+# 1차 시도: 적은 길이 문자열 예시를 miss
+# S = input()
+from itertools import permutations
+
+def func(S):
+    answer = S[:2]
+    for i in range(1,len(S)-1):
+        # S = min(S[:i], "".join(reversed(S[:i]))) + S[i:]
+        if answer[-1] == min(answer)\
+            or (answer[0] == min(answer) and S[i+1] < min(answer)):
+            answer = "".join(reversed(answer))
+        answer += S[i+1]
+
+    print(min(answer, "".join(reversed(answer))))
+
+
+permutations(list(map()))
