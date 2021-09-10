3f519f9faec2565afe459fac917ea418414772ea
ij_slango <junginje@gmail.com>
2021-09-11 00:39:07 +0900

    210911-알고-Gen

--- 00.Algorithm/Backjoon/DataStructure/1717.집합의 표현.py
+++ 00.Algorithm/Backjoon/DataStructure/1717.집합의 표현.py
@@ -1,43 +1,34 @@
+# 두번째 시도:
+# 이와 같이 양쪽의 포함 원소수를 비교하고 적은 것을 많은 것
+# 기준으로 바꾸면 루트를 찾아갈 필요없이 바로 roots배열에서 값 확인만 하면 된다.
+# 가 아니라 기존의 부모형태로 찾는 것이 맞다.
 import sys
-sys.setrecursionlimit(int(1e6))
+sys.setrecursionlimit(10**8)
 input = sys.stdin.readline
 
-# 첫번째 시도: 메모리 초과
-# 연속적으로 모든 수들이 연결되었다면 호출시 10만번
-# 호출되면서 메모리+시간 초과 가능성
-roots = []
-
-def unif(a, b):
-    sml, big = min(a,b), max(a,b)
-    # if roots[big] != roots[sml]:
-    #     roots[big] = roots[sml]
-    if roots[getrt(big)] != roots[sml]:
-        roots[getrt(big)] = roots[sml]
-
 def getrt(a):
-    if roots[a] == a:
+    if roots[a] == a:    
         return a
     return getrt(roots[a])
 
-def unifchk(a, b):
-    # ga, gb = getrt(a), getrt(b)
-    # return ga == gb
-    return getrt(a) == getrt(b)
+def unif(a, b):
+    ra = getrt(a)
+    rb = getrt(b)
+    if ra < rb:
+        roots[rb] = ra
+    else:
+        roots[ra] = rb
 
-def main():
-    global roots
-    n, m = map(int, input().split())
-    roots = [i for i in range(n+1)]
-    for _ in range(m):
-        func, a, b = map(int, input().split())
-        if func == 0:
-            unif(a, b)
-        elif unifchk(a, b):
-            print('YES')
-        else:
-            print('NO')
-
-main()
+n, m = map(int, input().split())
+roots = [i for i in range(n+1)]
+for _ in range(m):
+    func, a, b = map(int, input().split())
+    if func == 0:
+        unif(a, b, n)
+    elif getrt(a) == getrt(b):
+        print('YES')
+    else:
+        print('NO')
 
 
 """
@@ -70,3 +61,40 @@ main()
 1 0 2
 
 """
+
+# # 첫번째 시도: 메모리 초과
+# # 연속적으로 모든 수들이 연결되었다면 호출시 10만번
+# # 호출되면서 메모리+시간 초과 가능성
+# roots = []
+
+# def unif(a, b):
+#     sml, big = min(a,b), max(a,b)
+#     # if roots[big] != roots[sml]:
+#     #     roots[big] = roots[sml]
+#     if roots[getrt(big)] != roots[sml]:
+#         roots[getrt(big)] = roots[sml]
+
+# def getrt(a):
+#     if roots[a] == a:
+#         return a
+#     return getrt(roots[a])
+
+# def unifchk(a, b):
+#     # ga, gb = getrt(a), getrt(b)
+#     # return ga == gb
+#     return getrt(a) == getrt(b)
+
+# def main():
+#     global roots
+#     n, m = map(int, input().split())
+#     roots = [i for i in range(n+1)]
+#     for _ in range(m):
+#         func, a, b = map(int, input().split())
+#         if func == 0:
+#             unif(a, b)
+#         elif unifchk(a, b):
+#             print('YES')
+#         else:
+#             print('NO')
+
+# main()
