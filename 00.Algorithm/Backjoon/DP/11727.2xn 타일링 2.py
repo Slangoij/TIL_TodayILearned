e7d297b2edf32641ff72553d942e2dd4ca468b21
ij_slango <junginje@gmail.com>
2021-10-04 16:51:22 +0900

    211004-알고-Gen

--- 00.Algorithm/Backjoon/DP/11727.2xn 타일링 2.py
+++ 00.Algorithm/Backjoon/DP/11727.2xn 타일링 2.py
@@ -1,5 +1,5 @@
-dp = [[1,0],[1,2]]
+dp = [0,1,3]
 n = int(input())
 for i in range(3,n+1):
-    dp.append([sum(dp[i-2]), dp[i-3][0]*2])
-print(sum(dp[n-1]))
+    dp.append((dp[i-1] + dp[i-2]*2)%10007)
+print(dp[n])
