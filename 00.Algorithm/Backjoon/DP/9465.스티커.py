a36e8f3520ef0a16803c5a15dd1705829e3484c6
ij_slango <junginje@gmail.com>
2021-10-16 23:41:10 +0900

    211016-알고-Gen

--- 00.Algorithm/Backjoon/DP/9465.스티커.py
+++ 00.Algorithm/Backjoon/DP/9465.스티커.py
@@ -6,7 +6,10 @@ for _ in range(int(input())):
     mapp = []
     for _ in range(2):
         mapp.append(list(map(int, input().split())))
-    dp = [[mapp[0][0], mapp[1][0]], [mapp[1][0]+mapp[0][1], mapp[0][0]+mapp[1][1]]]
+    if n == 1:
+        dp = [[mapp[0][0], mapp[1][0]]]
+    else:
+        dp = [[mapp[0][0], mapp[1][0]], [mapp[1][0]+mapp[0][1], mapp[0][0]+mapp[1][1]]]
     for i in range(2,n):
         dp.append([max(dp[i-2][1], dp[i-1][1])+mapp[0][i],
                    max(dp[i-2][0], dp[i-1][0])+mapp[1][i]])
