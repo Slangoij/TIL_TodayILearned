94f68d3fd4bbeb9608eaf68a91e7c113edb93e03
ij_slango <junginje@gmail.com>
2021-12-07 21:27:17 +0900

    211205-알고-Gen

--- 00.Algorithm/Backjoon/DP/2293.동전 1.py
+++ 00.Algorithm/Backjoon/DP/2293.동전 1.py
@@ -1,9 +1,19 @@
-n,k = map(int, input().split())
-dp = [0]*(k+1)
+import sys
+input = sys.stdin.readline
+
+n, k = map(int, input().split())
+dp = [0] * (k+1)
 coins = []
 for _ in range(n):
     coins.append(int(input()))
-for i in range(2,k+1):
-    for j in range(i-1,(i-1)//2,-1):
-        if dp[i-j]:
-            dp[i] += dp[j]
+coins.sort()
+for i in range(1, k+1):
+    # for j in range(i-1, (i-1)//2, -1):
+    #     if dp[j] != 0 and i-j in coins:
+    #         dp[i] += dp[j]
+    for j in coins:
+        if i-j > 0 and dp[i-j]:
+            dp[i] += dp[i-j]
+    if i in coins:
+        dp[i] += 1
+print(dp[-1])
