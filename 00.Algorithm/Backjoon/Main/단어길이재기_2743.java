3c78b3389c6584acdd4206344dff425fd5b747ea
ij_slango <junginje@gmail.com>
2022-04-10 02:52:40 +0900

    중간수정_220410

---
+++ 00.Algorithm/Backjoon/Main/단어길이재기_2743.java
@@ -0,0 +1,10 @@
+package Main;
+import java.util.Scanner;
+
+public class 단어길이재기_2743(int[] args) {
+    String str;
+    int length;
+    Scanner sc = new Scanner(System.in);
+    str = sc.nextLine();
+    System.out.println(str.length());
+}
