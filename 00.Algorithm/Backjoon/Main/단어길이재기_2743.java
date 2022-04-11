e3426d93e358af5c97669a3eb488424c8b96dabc
ij_slango <junginje@gmail.com>
2022-04-11 23:21:04 +0900

    단어길이재기_java_수정및재커밋

--- 00.Algorithm/Backjoon/Main/단어길이재기_2743.java
+++ 00.Algorithm/Backjoon/Main/단어길이재기_2743.java
@@ -1,10 +1,12 @@
 package Main;
 import java.util.Scanner;
 
-public class 단어길이재기_2743(int[] args) {
-    String str;
-    int length;
-    Scanner sc = new Scanner(System.in);
-    str = sc.nextLine();
-    System.out.println(str.length());
+public class 단어길이재기_2743 {
+    public static void main(String[] args) {
+        String str;
+        Scanner sc = new Scanner(System.in);
+        str = sc.nextLine();
+        System.out.println(str.length());
+        sc.close();
+    }
 }
