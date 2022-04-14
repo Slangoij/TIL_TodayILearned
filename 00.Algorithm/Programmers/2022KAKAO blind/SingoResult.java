89334b7ec4d635a684d285d928cb2122ccbb4d1c
ij_slango <junginje@gmail.com>
2022-04-15 01:09:49 +0900

    카카오2022_신고결과받기 문제 풀이중

---
+++ 00.Algorithm/Programmers/2022KAKAO blind/SingoResult.java
@@ -0,0 +1,16 @@
+import java.util.HashMap;
+
+class Solution {
+    public int[] solution(String[] report, String[] reported, int k) {
+        int[] answer = {};
+        int whole_num = report.length;
+
+        int[] reported = new int[report.length];
+        for (int i=0;i<report.length;i++){
+
+        }
+
+
+        return answer;
+    }
+}
